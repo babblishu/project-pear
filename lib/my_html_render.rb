6be6b267cf6a0b161a96b4589dfdfdfe36c72fd3
Crash <blue.boy.in.china@gmail.com>
2013-03-09 17:45:28 +0800

    20130309

--- lib/my_html_render.rb
+++ lib/my_html_render.rb
@@ -1,5 +1,13 @@
+require 'cgi'
+
 class MyHTMLRender < Redcarpet::Render::HTML
   def block_code(code, language)
-    "<pre class=\"prettyprint lang-#{language}\">\n#{code.gsub(/\t/, '    ')}</pre>"
+    code = CGI::escapeHTML code
+    if language
+      "<pre class=\"prettyprint lang-#{language}\">\n#{code.gsub(/\t/, '    ')}</pre>"
+    else
+      "<pre>\n#{code.gsub(/\t/, '    ')}</pre>"
+    end
+
   end
 end
