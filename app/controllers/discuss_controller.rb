7c93c894dc3a12a54b62e09df2507f286ecc03de
Crash <blue.boy.in.china@gmail.com>
2013-03-12 19:54:32 +0800

    20130312

--- app/controllers/discuss_controller.rb
+++ app/controllers/discuss_controller.rb
@@ -3,7 +3,7 @@ require 'cgi'
 class DiscussMarkdownHTMLRender < Redcarpet::Render::HTML
   def block_code(code, language)
     code = CGI::escapeHTML code
-    if APP_CONFIG.program_languages.keys.include? language
+    if APP_CONFIG.program_languages.keys.map(&:to_s).include? language
       "<pre class=\"prettyprint lang-#{language}\">\n#{code.gsub(/\t/, '    ')}</pre>"
     else
       "<pre>\n#{code.gsub(/\t/, '    ')}</pre>"
