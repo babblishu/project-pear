e12d6cd19a5cacabf48e4b506614020bc86dc646
Crash <blue.boy.in.china@gmail.com>
2013-04-07 17:58:37 +0800

    20130407

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,10 +1,10 @@
 module SubmissionsHelper
   def result_cache_name
     if @current_user
-      if @current_user.id == @submission.user_id
-        'self'
-      elsif @current_user.role == 'admin'
+      if @current_user.role == 'admin'
         'admin'
+      elsif @current_user.id == @submission.user_id
+        'self'
       else
         'normal'
       end
